81ca93d1b774e299b98581c09ae69693b5a8aa05
sdldev <sidolapakdev@gmail.com>
2024-05-29 09:09:33 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$host = "localhost";
+$databaseuser = "u1447849_rdm";
+$databasename = "u1447849_rdm";
+$dbpassword = "rdm_mis@123";
+define("DB_SERVER",$host);
+define("DB_USERNAME",$databaseuser);
+define("DB_PASSWORD",$dbpassword);
+define("DB_DATABASE",$databasename);
+?>
